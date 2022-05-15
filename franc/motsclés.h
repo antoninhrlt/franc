940b1653a37289b71d488baa4308a1a021c07521
antoninhrlt <antoninhrlt1@gmail.com>
2022-05-16 00:17:37 +0200

    Ajoute tous les mots clés

--- franc/motsclés.h
+++ franc/motsclés.h
@@ -7,7 +7,29 @@
 
 #define principale  main
 
+#define sortir      break
+#define poursuivre  continue
 #define renvoyer    return
+#define faire       do
+#define tantque     while
+#define allerà      goto
+#define si          if
+#define sinon       else
+#define pour        for
+#define pourchaque  pour
+#define selon       switch
+#define cas         case
+#define deftype     typedef
+#define taillede    sizeof
+#define registre    register
+// union
+#define unions      union
+#define structure   struct
+#define strutures   structure
+#define énumération     enum
+#define énumérations    énumération
+#define externe     extern
+#define externes    externe
 
 #endif // FRANC_MOTCLES
 
