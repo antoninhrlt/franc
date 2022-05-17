ab6b242b834bb2a7998882058bd62f9721a55eb3
antoninhrlt <antoninhrlt1@gmail.com>
2022-05-17 10:35:48 +0200

    Ajoute modes d'ouverture fichiers

--- franc/bst/flux.h
+++ franc/bst/flux.h
@@ -7,6 +7,30 @@
 
 #include <stdio.h>
 
+// Modes d'ouverture de fichiers
+/**
+ * Utilisation :
+ *  fopen(nom_fichier, <mode> <?BINAIRE>) : Pour un simple mode
+ *  fopen(nom_fichier, <mode_BINAIRE>) : Pour les doubles modes
+ * Exemples :
+ *  fopen("essai.txt", LIRE)
+ *  fopen("essai", LIRE BINAIRE)
+ *  fopen("essai", LIRE_ET_ECRIRE_BINAIRE)
+*/
+#define BINAIRE     "b"
+
+#define LIRE        "r"
+#define ECRIRE      "w"
+#define AJOUTER     "a"
+
+#define LIRE_ET_ECRIRE  "r+"
+#define ECRIRE_ET_LIRE  "w+"
+#define LIRE_ET_AJOUTER "a+"
+
+#define LIRE_ET_ECRIRE_BINAIRE  "rb+"
+#define ECRIRE_ET_LIRE_BINAIRE  "wb+"
+#define LIRE_ET_AJOUTER_BINAIRE "ab+"
+
 #define imprimer    printf
 #define lire        scanf
 #define récupérerc  getc
